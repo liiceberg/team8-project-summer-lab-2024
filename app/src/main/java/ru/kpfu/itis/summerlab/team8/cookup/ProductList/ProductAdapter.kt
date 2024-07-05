0644d16fc0bef9c2381121e02b5f4aeb3cea34a4
Ayrat <57062261+clovn@users.noreply.github.com>
2024-07-05 23:51:46 +0300

    product list logic

---
+++ app/src/main/java/ru/kpfu/itis/summerlab/team8/cookup/ProductList/ProductAdapter.kt
@@ -0,0 +1,25 @@
+package ru.kpfu.itis.summerlab.team8.cookup.ProductList
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import ru.kpfu.itis.summerlab.team8.cookup.databinding.ItemProductBinding
+
+class ProductAdapter (
+    private val list : MutableList<String>,
+) : RecyclerView.Adapter<ProductHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProductHolder {
+        return ProductHolder(ItemProductBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        ))
+    }
+
+    override fun getItemCount(): Int = list.size
+
+    override fun onBindViewHolder(holder: ProductHolder, position: Int) {
+        holder.onBind(list[position])
+    }
+
+}
--- app/src/main/java/ru/kpfu/itis/summerlab/team8/cookup/ProductListFragment.kt
+++
@@ -1,6 +0,0 @@
-package ru.kpfu.itis.summerlab.team8.cookup
-
-import androidx.fragment.app.Fragment
-
-class ProductListFragment : Fragment(R.layout.fragment_product_list) {
-}
