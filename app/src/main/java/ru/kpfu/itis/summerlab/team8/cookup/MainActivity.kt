05423f5c10579ff07a589ea517e64cce61ee15c4
yaildarr <ildartanki@mail.ru>
2024-07-06 19:08:26 +0300

    splash and logo done

--- app/src/main/java/ru/kpfu/itis/summerlab/team8/cookup/MainActivity.kt
+++ app/src/main/java/ru/kpfu/itis/summerlab/team8/cookup/MainActivity.kt
@@ -2,6 +2,7 @@ package ru.kpfu.itis.summerlab.team8.cookup
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
@@ -15,6 +16,9 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        installSplashScreen()
+
         viewBinding = ActivityMainBinding.inflate(layoutInflater)
 
         setContentView(viewBinding?.root)
