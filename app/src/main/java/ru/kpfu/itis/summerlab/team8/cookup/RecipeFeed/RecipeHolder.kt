ad137427dbffb2c94ce4791ff952e19a7ec46dae
yaildarr <ildartanki@mail.ru>
2024-07-06 17:11:18 +0300

    RecipeFeed logic done

---
+++ app/src/main/java/ru/kpfu/itis/summerlab/team8/cookup/RecipeFeed/RecipeHolder.kt
@@ -0,0 +1,29 @@
+package ru.kpfu.itis.summerlab.team8.cookup.RecipeFeed
+
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.RequestManager
+import ru.kpfu.itis.summerlab.team8.cookup.Recipe
+import ru.kpfu.itis.summerlab.team8.cookup.databinding.ItemRecipeFeedBinding
+
+class RecipeHolder (
+    private val glide: RequestManager,
+    private val binding: ItemRecipeFeedBinding,
+    private val click: (Long) -> Unit
+): RecyclerView.ViewHolder(binding.root) {
+    fun onBind(recipe: Recipe) {
+        binding.run {
+            textViewTitle.text = recipe.name
+            textViewDescription.text = recipe.description
+            glide.load(recipe.urlImage).into(imageViewRecipe)
+            root.setOnClickListener { click(recipe.id) }
+        }
+    }
+    fun bind(recipe: Recipe) {
+        binding.run {
+            textViewTitle.text = recipe.name
+            textViewDescription.text = recipe.description
+            glide.load(recipe.urlImage).into(imageViewRecipe)
+            root.setOnClickListener { click(recipe.id) }
+        }
+    }
+}
